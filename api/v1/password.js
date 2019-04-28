b68a83886bf1b47edfaf657eb8db8954162cf5ad
Joebert Jacaba <joebertj@gmail.com>
2019-04-28 09:53:28 +0000

    added request parameter

--- api/v1/password.js
+++ api/v1/password.js
@@ -1,12 +1,18 @@
 var assert = require('assert');
-
+var sprintf = require('sprintf-js').sprintf;
 const cassandra = require('cassandra-driver');
-const client = new cassandra.Client({ contactPoints: ['localhost'], localDataCenter: 'datacenter1', keyspace: 'user' });
+var authProvider = new cassandra.auth.PlainTextAuthProvider('cassandra', 'cassandra');
+const client = new cassandra.Client({ contactPoints: ['localhost'], localDataCenter: 'datacenter1', keyspace: 'user', authProvider: authProvider });
 
 exports.find = function(req, res){
-    const query = 'SELECT count FROM users WHERE shaone = ?';
-    client.execute(query, [ 'someone' ], function(err, result) {
+    const query = 'SELECT count FROM password WHERE shaone = ?';
+    var shaone = req.query.shaone;	
+    client.execute(query, [ shaone ], function(err, result) {
         assert.ifError(err);
-        console.log('Breached %s times', result.rows[0].count);
+	if(result.rows.length == 1) {
+            res.end(sprintf('{ "count" : %s}', result.rows[0].count));
+	}else{
+            res.end(sprintf('{ "count" : 0}'));
+	}
     });
 }
