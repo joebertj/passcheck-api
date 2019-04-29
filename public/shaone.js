d42aa61d94e2a2640fe0d1d26cfb703950259a85
Joebert Jacaba <joebertj@gmail.com>
2019-04-29 17:22:24 +0000

    added jquery ajax

--- public/shaone.js
+++ public/shaone.js
@@ -1,5 +1,16 @@
 function hashPass(){
-    var shaone = CryptoJS.SHA1(document.getElementsByName("shaone")[1].value);	
+    var shaone = CryptoJS.SHA1(document.passform.plain.value);	
     document.passform.shaone.value = shaone.toString().toUpperCase();
-    document.passform.submit();
+    $( "#result" ).empty().append("Searching...");
+ 
+    var posting = $.post( "/api/v1/password", { shaone: document.passform.shaone.value } );
+ 
+    posting.done(function( data ) {
+	var p = JSON.parse(data);
+	if(p.count == 0){
+	    $( "#result" ).empty().append( "Your password is safe" );
+	}else{
+	    $( "#result" ).empty().append( "Your password has been part of a breach " + p.count + " times!");
+	}
+    });
 }
